c97a5d9eacf35e7c14c9a6ccb7b95d2e50160612
BlexoC <makaublessing929@gmail.com>
2026-06-08 15:15:26 +0300

    Refactor task management structure; remove obsolete files and implement validation in task_manager

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from task_utils import add_task, mark_task_as_complete, view_pending_tasks, calculate_progress
-from validation import validate_task_title, validate_task_description, validate_due_date# Define the main function
+from task_manager.task_utils import add_task, mark_task_as_complete, view_pending_tasks, calculate_progress, tasks
 def main():
     while True:
         print("Task Management System")
