4cb68d192adbd04c8016d77a947b3346f2fc493b
BlexoC <makaublessing929@gmail.com>
2026-06-08 11:33:21 +0300

    Refactor main and task_utils functions; improve code formatting and readability

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import functions from task_manager.task_utils package
 None
 
+
 # Define the main function
 def main():
     while True:
@@ -11,17 +12,18 @@ def main():
         print("4. View Progress")
         print("5. Exit")
         choice = input("Enter your choice (1-5): ")
-
         if choice == "1":
             None
-        None
-        None
-        None
+
+            None
+            None
+            None
         elif choice == "5":
             print("Exiting the program...")
             break
         else:
             print("Invalid choice. Please try again.")
-        
+
+
 if __name__ == "__main__":
     main()
